4653c2b3a201c10fbd1bf373950f15bc4abd1d32
natekohl <nate@natekohl.net>
2011-12-28 20:54:34 -0500

    another constructor or two

--- Point2D.h
+++ Point2D.h
@@ -1,9 +1,25 @@
 #ifndef _POINT_2D_H
 #define _POINT_2D_H
 
+struct AngDeg {
+  AngDeg(double t) : t(t) {}
+  double t;
+};
+
+struct AngRad {
+  AngRad(double t) : t(t) {}
+  double t;
+};
+
 class Point2D {
 public:
   Point2D(double x, double y) : m_x(x), m_y(y) {}
+  Point2D(double radius, AngDeg angDeg) {
+    std::cout << "degrees\n";
+  }
+  Point2D(double radius, AngRad angRad) {
+    std::cout << "radians\n";
+  }
 
 private:
   double m_x, m_y;
--- main.cc
+++ main.cc
@@ -2,5 +2,5 @@
 #include "Point2D.h"
 
 int main() {
-  Point2D p(3, 1.41);
+  Point2D p(3, 45);
 }
